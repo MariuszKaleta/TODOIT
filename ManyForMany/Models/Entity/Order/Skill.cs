9a3ab30d0d86c6db56234ad1e8050f5959a1cb6c
Mariusz Kaleta <marjan.kaleta@gmail.com>
2019-06-23 15:17:05 +0200

    [feature] Add Skills model

---
+++ ManyForMany/Models/Entity/Order/Skill.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcHelper.Entity;
+
+namespace ManyForMany.Models.Entity.Order
+{
+    public class Skill : IId<int>
+    {
+        private Skill()
+        {
+
+        }
+
+        public Skill(string name)
+        {
+            Name = name;
+        }
+
+        [Key]
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+    }
+}
